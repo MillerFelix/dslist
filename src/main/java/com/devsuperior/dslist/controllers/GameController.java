9013bbc075e25366a0f52f5f1a22a605861ca0a0
Miller <millerredigolo@gmail.com>
2023-07-12 09:06:50 -0300

    Creating GameController, GameMinDto, GameRepository, GameService

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameMinDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.services.GameService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDto> findAll() {
+        List<GameMinDto> result = gameService.findAll();
+        return result;
+    }
+}
